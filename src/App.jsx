8cdfcd2616ba39cdb0d97a67c5b449a7bd6a3781
Vansh Verma <vanshverma1117@gmail.com>
2026-06-09 00:13:23 +0530

    feat: add App and main entry points

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
