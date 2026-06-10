701d249a82a502e53e9692cd17d70007c34ca30a
Vansh Verma <vanshverma1117@gmail.com>
2026-06-10 23:54:35 +0530

    feat: update App.jsx with routing and page imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-// import Home from './Pages/Home';
-import Shop from './Pages/Shop';
+import Home from './Pages/Home';
+// import Shop from './Pages/Shop';
 
 function App() {
   return (
     <div className="App">
-      <Shop />
+      {/* <Shop /> */}
+     <Home />
     </div>
   );
 }
